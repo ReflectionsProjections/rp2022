6649e931787ee2f1c0e1f8a301c186d3519498ff
Armani <Armanichien2021@gmail.com>
2022-05-08 00:30:50 -0500

    Gradient colors changed from (pink, white) to (pink, light pink))

--- services/handleMouseMove.js
+++ services/handleMouseMove.js
@@ -3,12 +3,12 @@ export default function handleMouseMove(event) {
   const height = window.outerHeight;
   const xOffset = 16;
   const yOffset = 3;
-  const mouseXpercentage = (event.clientX / width) * 100; //Math.round(event.pageX / width * 100);
-  const mouseYpercentage = (event.clientY / height) * 100 + yOffset; //Math.round(event.pageY / height * 100);
+  const mouseXpercentage = (event.clientX / width) * 65; //Math.round(event.pageX / width * 100);
+  const mouseYpercentage = (event.clientY / height) * 75 + yOffset; //Math.round(event.pageY / height * 100);
   document.getElementsByTagName('body')[0].style =
     'background: radial-gradient(at ' +
     mouseXpercentage +
     '% ' +
     mouseYpercentage +
-    '%, #EE6350, #ffffff)';
+    '%, #EE6350, #F9E3E0)';
 }
