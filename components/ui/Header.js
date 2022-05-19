0bbf4c552f800254d8abbd3ada9fa0a1a79077ca
unknown <gagan2@illinois.edu>
2022-05-19 14:02:38 -0500

    Added some responsiveness.

--- components/ui/Header.js
+++ components/ui/Header.js
@@ -5,6 +5,7 @@ import { AppBar, Toolbar, Typography, Button, Grid, Box } from '@mui/material';
 
 export default function Header({ isHero }) {
     const small_logo = '/rp_logo_2022_sm.png';
+    const small_logo_no_bg = '/rp_logo_2022_no_bg.png';
     const pagesData = [
         {
             label: 'About',
@@ -27,11 +28,11 @@ export default function Header({ isHero }) {
 
     return (
         <>
-            <Box className={styles.headerBox}>
-                <AppBar className={isHero ? styles.hero : styles.header}>
+            {/* <Box className={styles.headerBox}> */}
+                <AppBar className={!isHero ? styles.hero : styles.header}>
                     
-                    <Toolbar>
-                        <Grid container spacing={3} alignItems="center" justifyContent="space-between" display="flex">
+                    {/* <Toolbar className={styles.toolbar}> */}
+                        <Grid container spacing={12} alignItems="center" display="flex">
                             <Grid item md={4} zeroMinWidth justifySelf="flex-start">
                                 <div className={styles.logoBox}>
                                     <img src={small_logo} alt='2022 R|P Logo' className={styles.logo} />
@@ -40,9 +41,9 @@ export default function Header({ isHero }) {
 
                             <Grid item md={4}>
                                 <div className={styles.pages}>
-                                    {pagesData.map(({ label, href }) => (
+                                    {pagesData.map(({ label, href }, idx) => (
                                         <Link href={href} passHref>
-                                            <a rel="noreferrer" className={styles.page}>
+                                            <a rel="noreferrer" className={styles.page} style={{order: {idx}}}>
                                                 <Typography>
                                                     {label}
                                                 </Typography>
@@ -52,23 +53,23 @@ export default function Header({ isHero }) {
                                 </div>
                             </Grid>
                             
-                            <Grid item md={4} zeroMinWidth>
+                            <Grid item md={4}>
                                 <div className={styles.navButtons}>
-                                    <Button variant='contained' className={styles.navButton} style={{ backgroundColor: '#41798C'}} disableElevation>
+                                    <Button variant='contained' className={styles.navButton} style={{ backgroundColor: '#41798C', order: '0'}} disableElevation>
                                         Register
                                     </Button>
 
                                     <Button variant='contained' 
                                             className={styles.navButton} 
-                                            style={{ backgroundColor: 'transparent', border: '2px solid #FFFFFF'}} disableElevation>
+                                            style={{ backgroundColor: 'transparent', border: '2px solid #FFFFFF', order: '1'}} disableElevation>
                                         Contact Us
                                     </Button>
                                 </div>
                             </Grid>
                         </Grid>
-                    </Toolbar>
+                    {/* </Toolbar> */}
                 </AppBar>
-            </Box>
+            {/* </Box> */}
         </>
     )
 }
