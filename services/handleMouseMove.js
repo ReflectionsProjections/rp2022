660e8d9c4e8118410df8cdbb8ad736a45ca55000
Armani <Armanichien2021@gmail.com>
2022-05-25 19:52:51 -0500

    Replaced mouse tracking with static background + added grain overlay (Imported white stroke but can't figure out how to get it to not take up document flow)

--- services/handleMouseMove.js
+++ services/handleMouseMove.js
@@ -6,9 +6,12 @@ export default function handleMouseMove(event) {
   const mouseXpercentage = (event.clientX / width) * 100; //Math.round(event.pageX / width * 100);
   const mouseYpercentage = (event.clientY / height) * 75 + yOffset; //Math.round(event.pageY / height * 100);
   document.getElementsByTagName('body')[0].style =
-    'background: radial-gradient(at ' +
-    mouseXpercentage +
-    '% ' +
-    mouseYpercentage +
-    '%, #EE6350, #F9E3E0)';
+    'background:' + 'url("resources/white_grain.png") '
+    // + ' radial-gradient(at ' +
+    // mouseXpercentage +
+    // '% ' +
+    // mouseYpercentage +
+    // '%, #EE6350, #F9E3E0)'
+    ;
+    // Background mouse tracking commented out & replaced with white grain overlay
 }
