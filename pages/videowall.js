9a841426792cc9a1cc3423e2895f4c4defb15713
atharvanaik <atharvanaik10@gmail.com>
2022-05-27 19:16:35 -0400

    Fix deployment bugs

--- pages/videowall.js
+++ pages/videowall.js
@@ -1,11 +1,11 @@
 import Dashboard from '../components/Dashboard';
-import handleMouseMove from '../services/handleMouseMove';
+//import handleMouseMove from '../services/handleMouseMove';
 
 export default function DB() {
   if (typeof window !== 'undefined') {
     document.getElementsByTagName('body')[0].style =
       'background: linear-gradient(var(--yellow-2), var(--yellow-1));';
-    window.removeEventListener('mousemove', handleMouseMove);
+    //window.removeEventListener('mousemove', handleMouseMove);
   }
 
   return <Dashboard />;
