fa870e177abd18d2857ebb65349d39566b262a12
Apoorva Aditya <apoorvaufo3@gmail.com>
2022-06-07 15:17:50 +0530

    added svg with all the ripples and blue wave

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,7 @@ export default function Index() {
       <div className={styles.bg}>
         {/* <img class = "fixed-to-top" src="/resources/MiddleWhiteStrokeBackground.png" alt="White Stroke" /> 
         # How can I make this white line not take up document flow (space) */}
-        <img className = "bg-img" src="/resources/BlueWavyBackground.png" alt="Blue Wavy Background" />
+        <img className = "bg-img" src="/resources/Blue Curve.svg" alt="Blue Wavy Background" />
       </div>
     </>
   );
