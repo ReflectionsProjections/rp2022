759c00f15e4b52e74d9ca39b0ad18156a941fbe8
unknown <gagan2@illinois.edu>
2022-06-18 18:03:24 -0500

    Created EventCard component

---
+++ components/Schedule/EventCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { useStyles } from './EventCardStyles.js';
+
+export default function EventCard({startTime, endTime, title, description}) {
+    const classes = useStyles();
+    return(
+        <>
+            <Box className={classes.card}>
+                <Box className={classes.headerBox}>
+                    <div className={classes.dot}></div>
+                    <Typography className={classes.time}>{startTime} - {endTime}</Typography>
+                </Box>
+
+                <Box className={classes.textBox}>
+                    <Typography className={classes.title}>{title}</Typography>
+                    <Typography className={classes.description}>{description}</Typography>
+                </Box>
+            </Box>
+        </>
+    )
+}
