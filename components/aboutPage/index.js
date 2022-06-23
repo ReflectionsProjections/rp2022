25de647a9c1265c0e32ea41254586c577d20a443
Armani <Armanichien2021@gmail.com>
2022-06-22 20:14:48 -0500

    AboutPage Content & Skeleton Code

---
+++ components/aboutPage/index.js
@@ -0,0 +1,19 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Section from '../Section';
+
+import AvatarCard from '../ui/AvatarCard';
+// import SpeakerCard from './components/SpeakerCard';
+
+const About = () => {
+  return (
+    <span>About Page</span>
+    <Box>
+        <Typography></Typography>
+    </Box>
+  );
+};
+
+export default About;
