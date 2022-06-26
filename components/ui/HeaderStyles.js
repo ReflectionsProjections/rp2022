c8f6fe820b49e6d95b6164205293b3cced9c97f1
unknown <gagan2@illinois.edu>
2022-06-26 12:12:09 -0500

    Updated header design and adjusted About component position.

--- components/ui/HeaderStyles.js
+++ components/ui/HeaderStyles.js
@@ -1,19 +1,26 @@
 import { makeStyles } from '@mui/styles';
+import redCurve from '../../public/resources/red_curve.svg';
+
+const tablet = '850px';
+const mobile = '420px';
 
 const useStyles = makeStyles({
-    hero: {
-        position: 'static',
-        width: '100%',
-        zIndex: 999,
-
-        display: 'flex',
-        padding: '2rem',
-        background: 'transparent',
-        boxShadow: 'none',
+    curve: {
+        position: "fixed",
+        width: "100%",
+        height: 'auto',
         top: 0,
         left: 0,
+
+        [`@media (max-width: ${tablet})`]: {
+            transform: "scale(1.25)"
+        },
+
+        [`@media (max-width: ${mobile})`]: {
+            display: "none"
+        },
     },
-    
+
     header: {
         position: 'fixed',
         width: '100%',
@@ -22,7 +29,11 @@ const useStyles = makeStyles({
         display: 'flex',
         padding: '2rem',
         background: 'transparent',
-        boxShadow: '0px 16px 60px rgba(0, 0, 0, 0.15)',
+        [`@media (max-width: ${mobile})`]: {
+            background: "radial-gradient(101.76% 724.4% at 51.92% 208.23%, #EE6350 0%, #F9E3E0 100%)",
+        },
+        opacity: 0.9,
+        boxShadow: 'none',
         top: 0,
         left: 0,
     },
@@ -49,6 +60,11 @@ const useStyles = makeStyles({
         height: '78px',
         left: '0px',
         top: '0px',
+
+        [`@media (max-width: ${mobile})`]: {
+            width: '60px',
+            height: '60px',
+        },
 
         flex: 'none',
         order: 0,
@@ -85,6 +101,7 @@ const useStyles = makeStyles({
         
         '&:hover, &:visited, &:focus, &:active': {
             textDecoration: 'none',
+            borderBottom: "3px solid",
             cursor: 'pointer',
             color: 'white',
             marginLeft: '38px',
