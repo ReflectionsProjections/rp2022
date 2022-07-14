7d3bbd02c9bbe45dda14c064d38baee2cfe55df4
atharvanaik <atharvanaik10@gmail.com>
2022-07-13 21:25:52 -0400

    Fix sponsor terms

---
+++ pages/sponsorterms.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import PDFViewer from '../UIComponents/PDFViewer';
+
+const SponsorTerms = () => <PDFViewer filepath="/sponsorterms.pdf" />;
+
+export default SponsorTerms;
