23f47b5d20d71fa03c41c35e67aa7db35aa2a077
SRotiwar <shaarav.rotiwar@gmail.com>
2022-07-17 19:56:43 -0500

    Working on landing page

--- components/About.js
+++ components/About.js
@@ -55,6 +55,80 @@ export default function About() {
           */}
         </Container>
       </Col>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      
+      
+      <Col className={classes.mainCol}>
+
+        <p className={classes.subheading2}>
+        What is Reflections | Projections?
+        </p>
+        <p className={classes.diffBody}>
+        Reflections | Projections is a tech conference organized and run by students at  
+        <br></br>the University of Illinois at Urbana-Champaign. 
+
+        <br></br>
+          <br></br>
+       
+        We draw students, speakers, and companies from around the world. Join us to <br></br> listen to tech talks from across the industry, to participate in an Artificial <br></br> Intelligence programming competition in Mechmania, to solve puzzles <br></br>throughout the conference during PuzzleBang!, and to attend our job and <br></br>startup fairs. 
+          <br></br>
+          <br></br>
+          The conference is free and open to anyone regardless of age, major, job, or <br></br> affiliation with the University of Illinois. So come join a discussion led by some <br></br>of the brightest minds in the business about the bleeding edge of the tech <br></br> industry!
+        </p>
+        <Container className={classes.container}>
+          
+          <Button variant='contained'
+            className={classes.navButton2}
+            style={{ backgroundColor: '#41798C' }}>
+            Learn more
+          </Button>
+        </Container>
+        
+      </Col>
+
+      <Col className={classes.mainCol}>
+
+        <p className={classes.subheadingBold2}>
+        Prepare for 5 days full of events and tech!
+        </p>
+        <p className={classes.diffBody}>
+        Reflections | Projections is a tech conference organized and run by students at  
+        <br></br>the University of Illinois at Urbana-Champaign. 
+
+        <br></br>
+          <br></br>
+       
+        We draw students, speakers, and companies from around the world. Join us to <br></br> listen to tech talks from across the industry, to participate in an Artificial <br></br> Intelligence programming competition in Mechmania, to solve puzzles <br></br>throughout the conference during PuzzleBang!, and to attend our job and <br></br>startup fairs. 
+          <br></br>
+          <br></br>
+          The conference is free and open to anyone regardless of age, major, job, or <br></br> affiliation with the University of Illinois. So come join a discussion led by some <br></br>of the brightest minds in the business about the bleeding edge of the tech <br></br> industry!
+        </p>
+        <Container className={classes.container}>
+          
+          <Button variant='contained'
+            className={classes.navButton2}
+            style={{ backgroundColor: '#41798C' }}>
+            View schedule
+          </Button>
+        </Container>
+
+        <Container className={classes.container}>
+          
+          <Button variant='contained'
+            className={classes.navButton2}
+            style={{ backgroundColor: '#41798C' }}>
+            View schedule
+          </Button>
+        </Container>
+        
+      </Col>
     </>
     // </Container>
   );
