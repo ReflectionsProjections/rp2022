04563215f17abcc1401ed08f70ae9a0abc3d1edd
unknown <gagan2@illinois.edu>
2022-08-04 18:29:17 -0500

    temp fix

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import styles from '../styles/Landing.module.scss';
 import useGetStaticData from '../services/useGetStaticData';
 import SponsorSection from '../components/SponsorSection.js';
 import Nav from '../components/ui/Nav';
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import Header from '../components/ui/Header';
 import Footer from '../components/ui/Footer'
 import { Google } from 'react-bootstrap-icons';
@@ -22,137 +22,136 @@ export default function Index() {
 
   return (
     <>
-    <div className={styles.backgroundRed} >
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      {/* <Nav /> */}
-      <Header isHero={true}/>
-      
-      <div className={styles.landingContainer}>
-       {/* <main className={styles.main}>
+      <div className={styles.backgroundRed} >
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        {/* <Nav /> */}
+        <Header isHero={true} />
+
+        <div className={styles.landingContainer}>
+          {/* <main className={styles.main}>
           <img src="/rp22-logo.png" alt="Reflections Projections Logo" />
 
           <a href="https://airtable.com/shrTGIqGrMhlD32NC">
             <Button>Register Now!</Button>
           </a>
         </main> */}
-        
-          <About name="about" />     
-      </div>
 
-      
-      {/* <div className={styles.backgroundBlue}></div>  */}
-      <div className={styles.landingContainer}>
-        <Col className={styles.mainCol}>
-          <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-          <p className={styles.subheading2}>
-          What is Reflections | Projections?
-          </p>
-          <p className={styles.diffBody}>
-          Reflections | Projections is a tech conference organized and run by students at  
-          <br></br>the University of Illinois at Urbana-Champaign. 
-
-          <br></br>
-            <br></br>
-
-          We draw students, speakers, and companies from around the world. Join us to <br></br> listen to tech talks from across the industry, to participate in an Artificial <br></br> Intelligence programming competition in Mechmania, to solve puzzles <br></br>throughout the conference during PuzzleBang!, and to attend our job and <br></br>startup fairs. 
-            <br></br>
-            <br></br>
-            The conference is free and open to anyone regardless of age, major, job, or <br></br> affiliation with the University of Illinois. So come join a discussion led by some <br></br>of the brightest minds in the business about the bleeding edge of the tech <br></br> industry!
-          </p>
-          <Container className={styles.container}>
-            
-            <Button variant='contained'
-              className={styles.navButton2}
-              style={{ backgroundColor: '#41798C', disabled: true }} disableElevation>
-              Learn more
-            </Button>
-            <br></br><br></br><br></br><br></br>
-          </Container>
-
-        </Col>
-        
-        
-
-      </div> 
-
-        
-
-      {/* <div className={styles.backgroundPinkBlock}> */}
-      <div className={styles.backgroundPink}>  
-        <Col className={styles.mainCol} style={{display: 'flex'}}>
-          <p className={styles.subheadingBold2} style={{flex: 1, marginTop: '30px'}}>
-            Prepare for 5 days full of events and tech!
-          </p>
-          <p className={styles.diffBody} style={{marginLeft: '60vw', float: 'right', flex: 1}}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
-        <div className={styles.bg}>
-        <img className = "bg-img" 
-          style={{height: 560, width: 560, float: 'right', marginLeft: '30vw', marginRight: '20vw'}}
-          src={"/resources/vertical_line.svg"} alt="line"/>
+          <About name="about" />
         </div>
-        {/* <p className={styles.subheadingBold2} style={{marginLeft: '60vw', float: 'right'}}>
-        
-            Prepare for 5 days full of events and tech!
-          </p> */}
-        <Container className={styles.container}>
-          
-          <Button variant='contained'
-            className={styles.navButton2}
-            style={{ fontSize: '16px', backgroundColor: '#41798C', disabled: true }}>
-            View schedule
-          </Button>
-        </Container>
-        <br></br><br></br><br></br><br></br>
-        </Col>
-      </div>
-      {/* <SponsorSection sponsors={sponsors}/> */}
-      <Col className={styles.mainCol}>
+
+
+        {/* <div className={styles.backgroundBlue}></div>  */}
+        <div className={styles.landingContainer}>
+          <Col className={styles.mainCol}>
+            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+            <p className={styles.subheading2}>
+              What is Reflections | Projections?
+            </p>
+            <p className={styles.diffBody}>
+              Reflections | Projections is a tech conference organized and run by students at
+              <br></br>the University of Illinois at Urbana-Champaign.
+
+              <br></br>
+              <br></br>
+
+              We draw students, speakers, and companies from around the world. Join us to <br></br> listen to tech talks from across the industry, to participate in an Artificial <br></br> Intelligence programming competition in Mechmania, to solve puzzles <br></br>throughout the conference during PuzzleBang!, and to attend our job and <br></br>startup fairs.
+              <br></br>
+              <br></br>
+              The conference is free and open to anyone regardless of age, major, job, or <br></br> affiliation with the University of Illinois. So come join a discussion led by some <br></br>of the brightest minds in the business about the bleeding edge of the tech <br></br> industry!
+            </p>
+            <Container className={styles.container}>
+
+              <Button variant='contained'
+                className={styles.navButton2}
+                style={{ backgroundColor: '#41798C', disabled: true }} disableElevation>
+                Learn more
+              </Button>
+              <br></br><br></br><br></br><br></br>
+            </Container>
+
+          </Col>
+
+
+
+        </div>
+
+
+
+        {/* <div className={styles.backgroundPinkBlock}> */}
+        <div className={styles.backgroundPink}>
+          <Col className={styles.mainCol} style={{ display: 'flex' }}>
+            <div className={styles.scheduleContainer}>
+              <Typography className={styles.scheduleDesc1}>
+                Prepare for 5 days
+                full of events and tech!
+              </Typography>
+              <Button variant='contained'
+                className={styles.navButton2}
+                style={{ fontSize: '16px', backgroundColor: '#41798C', disabled: true }}>
+                View schedule
+              </Button>
+            </div>
+            <img
+              className={styles.middleLine}
+              src={"/resources/vertical_line.svg"} alt="line" />
+
+            <div className={styles.scheduleContainer2}>
+              <Typography className={styles.scheduleDesc2}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </Typography>
+            </div>
+          </Col>
+        </div>
+
+
+
+        {/* <SponsorSection sponsors={sponsors}/> */}
+        <Col className={styles.mainCol}>
 
           <p className={styles.subheading2}>
             Speakers
           </p>
           <p className={styles.diffBody}>
-        
+
             Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br></br> sed do eiusmod tempor incididunt ut labore et dolore <br></br>magna aliqua. Ut enim ad minim veniam, quis nostrud <br></br> exercitation ullamco laboris nisi ut aliquip ex ea commodo <br></br>consequat.
           </p>
-        <div className={styles.bg}>
-          <img className = "bg-img" 
-        // style={{height: 560, width: 560, marginTop: 570,marginLeft: 760}}
-          style={{height: 560, width: 560, float: 'right', marginLeft: '50vw', marginRight: '20vw'}}
-          src={"/resources/speaker.png"} alt="Speaker"/>
-       </div>
+          <div className={styles.bg}>
+            <img className="bg-img"
+              // style={{height: 560, width: 560, marginTop: 570,marginLeft: 760}}
+              style={{ height: 560, width: 560, float: 'right', marginLeft: '50vw', marginRight: '20vw' }}
+              src={"/resources/speaker.png"} alt="Speaker" />
+          </div>
           <Container className={styles.container}>
-            
+
             <Button variant='contained' disabled
               className={styles.navButton2}
-              style={{backgroundColor: '#41798C'}}>
+              style={{ backgroundColor: '#41798C' }}>
               Learn more
             </Button>
           </Container>
 
-  
-      </Col>
-      <Footer/>
-    </div>
-    <div className={styles.bgFixed}>
-        <img className="bg-img" src="/resources/Blue Curve.svg" alt="Blue Wavy Background"  />
-      </div>
-    
-  
 
-{/*  
+        </Col>
+        <Footer />
+      </div>
+      <div className={styles.bgFixed}>
+        <img className="bg-img" src="/resources/Blue Curve.svg" alt="Blue Wavy Background" />
+      </div>
+
+
+
+      {/*  
     <div className={styles.bg}>
 
       <img className = "bg-img" 
@@ -160,8 +159,8 @@ export default function Index() {
       
       src="/resources/Ripples.svg" alt="Radial circles" />
     </div> */}
-  
-    {/* <div>
+
+      {/* <div>
       <div className={styles.bg}>
       <img className = "bg-img" 
         // style={{height: 560, width: 560, marginTop: 570,marginLeft: 760}}
@@ -170,11 +169,11 @@ export default function Index() {
       </div>
       
     </div> */}
-    
+
     </>
   );
 }
- {/* <FAQ faqData={faqSection ?? { sections: [] }} />
+{/* <FAQ faqData={faqSection ?? { sections: [] }} />
         {sponsors && <SponsorSection sponsors={sponsors} />}
 
         <footer className={styles.footer}>
