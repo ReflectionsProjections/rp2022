e92dd200d4e4501d4e558cc011653b7631b07290
unknown <gagan2@illinois.edu>
2022-08-22 00:32:09 -0500

    Fixed css issues

--- components/SponsorSection.js
+++ components/SponsorSection.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Button, Typography } from '@mui/material';
 import styles from './SponsorSection.module.scss';
+import { useStyles } from './SponsorSectionStyles.js'
 import SponsorCard from './Sponsor/SponsorCard';
 
 const SponsorSection = ({ sponsors }) => {
+  const classes = useStyles();
+
   if (!sponsors) {
     return (
       <>
@@ -15,19 +18,19 @@ const SponsorSection = ({ sponsors }) => {
   return (
     <>
       <div className={styles.bg}>
-        <div className={styles.container}>
-          <div className={styles.headerBox}>
-            <Typography className={styles.header}>Sponsors</Typography>
-            <Typography className={styles.text}>Over 40+ sponsors support Reflections | Projections</Typography>
+        <div className={classes.container}>
+          <div className={classes.headerBox}>
+            <Typography className={classes.header}>Sponsors</Typography>
+            <Typography className={classes.text}>Over 40+ sponsors support Reflections | Projections</Typography>
           </div>
-          <div className={styles.content}>
-            <div className={styles.sponsors}>
+          <div className={classes.content}>
+            <div className={classes.sponsors}>
               {sponsors.map(({ key, name, img, website }) =>
                 <SponsorCard key={key} name={name} img={img} website={website} />
               )}
             </div>
             <Button variant='contained'
-              className={styles.button}
+              className={classes.button}
               href='/sponsor'
               style={{ fontSize: '16px', backgroundColor: '#41798C', disabled: true }}>
               View All Sponsors
